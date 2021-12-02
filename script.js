5b6f09442c4b9427b2a17acdb65898fbf27083e9
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-02 15:38:58 +0000

    open weather api integration

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-//1. Prevenir que o form seja enviado, quando carregado o botao search para que a pagina nao atualize.
-document.querySelector(".search").addEventListener("submit", (event) => {
-  event.preventDefault();
+document
+  .querySelector(".search")
+  .addEventListener("submit", async (/* codigo assincrono! */ event) => {
+    event.preventDefault();
 
-  //2. Assim que a pag nao foi atualizada, guardar o que foi colocado no input:
-  let input = document.querySelector("#searchInput").value;
-  console.log(input);
+    let input = document.querySelector("#searchInput").value;
+    console.log(input);
 
-  //3. Não dar search a um espaço em branco. Como?
-  if (input !== "") {
-    // loading... criar função para mostrar uma msg na .noResult
-    showMessage("Searching for results...");
-  } else {
-    document.querySelector(".noResult").innerHTML =
-      "There are no results for this search.";
-  }
-});
+    if (input !== "") {
+      showMessage("Searching for results...");
+
+      // URL do link da pagina Open Weather.
+      // Na template string, usar encodeURI para passar para um formato url
+      // API key = as vezes que vou acessar à api do site. Acessar a API key da minha conta
+      // existem outros parametros que podem ser usados p.e: lingua = &lang=en
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+        input
+      )}&appid=e643b5c2b7306a02cba630ffaadcda59&units=metric&lang=en`;
+
+      let results = await fetch(url); // fetch: promisses. fazer requisicao, esperar resposta, qd tiver resposta continua o codigo
+      let json = await results.json(); //await : ele diz"faz a requisicao,espera o resultado e TRANSFORMA em json!!""
+
+      console.log(json);
+    } else {
+      document.querySelector(".noResult").innerHTML =
+        "There are no results for this search.";
+    }
+  });
 function showMessage(msg) {
   document.querySelector(".noResult").innerHTML = msg;
 }

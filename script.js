19cc6a822e17d9d09ddc3f536837e165d781cea7
Carolina Strecht <carolina.stsampaio@gmail.com>
2021-12-02 16:39:57 +0000

    api request success and fail, html classes update

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-document
-  .querySelector(".search")
-  .addEventListener("submit", async (/* codigo assincrono! */ event) => {
-    event.preventDefault();
+document.querySelector(".search").addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    let input = document.querySelector("#searchInput").value;
-    console.log(input);
+  let input = document.querySelector("#searchInput").value;
+  console.log(input);
 
-    if (input !== "") {
-      showMessage("Searching for results...");
+  if (input !== "") {
+    showMessage("Searching for results...");
 
-      // URL do link da pagina Open Weather.
-      // Na template string, usar encodeURI para passar para um formato url
-      // API key = as vezes que vou acessar à api do site. Acessar a API key da minha conta
-      // existem outros parametros que podem ser usados p.e: lingua = &lang=en
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
-        input
-      )}&appid=e643b5c2b7306a02cba630ffaadcda59&units=metric&lang=en`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+      input
+    )}&appid=e643b5c2b7306a02cba630ffaadcda59&units=metric&lang=en`;
 
-      let results = await fetch(url); // fetch: promisses. fazer requisicao, esperar resposta, qd tiver resposta continua o codigo
-      let json = await results.json(); //await : ele diz"faz a requisicao,espera o resultado e TRANSFORMA em json!!""
+    let results = await fetch(url);
+    let json = await results.json();
 
-      console.log(json);
+    // API Open weather encontrou o resultado???
+    // RESULTADO EXISTE: json.cod = 200 ; RESULTADO NAO EXISTE: json.cod = '404' json.message='city not found'
+    if (json.cod === 200) {
+      // Se for verdade, mostrar as informacoes no ecra
+      showInfo({
+        name: json.name,
+        country: json.sys.country,
+        temp: json.main.temp,
+        tempIcon: json.weather[0].icon,
+        windSpeed: json.wind.speed,
+        windAngle: json.wind.deg,
+      });
     } else {
-      document.querySelector(".noResult").innerHTML =
-        "There are no results for this search.";
+      showMessage("There are no results for this search");
     }
-  });
+  } else {
+    document.querySelector(".noResult").innerHTML = "Type something please.";
+  }
+});
+
 function showMessage(msg) {
   document.querySelector(".noResult").innerHTML = msg;
+}
+
+function showInfo(json) {
+  showMessage(""); // retirar a show message
+  document.querySelector(".result").style.display = "block";
 }
